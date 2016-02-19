abe8b20ca88579c3c763766c3f3d1b257ac86d21
frank finger <franklinfinger@franks-MBP.attlocal.net>
2016-02-19 18:48:22 -0500

    updated layout and a little js

---
+++ photos.js
@@ -0,0 +1,13 @@
+var photos = [
+  {
+    albumName: "Album 1",
+    albumCover: 'photos/name-of-file.jpg',
+    photos: [
+      'photos/name-of-file1',
+      'photos/name-of-file2',
+      'photos/name-of-file3'
+    ]
+  },{
+    
+  }
+]
