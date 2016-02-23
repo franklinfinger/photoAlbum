80150f158c4be8e462695ca8fa99c5133dd07b04
frank finger <franklinfinger@franks-MBP.attlocal.net>
2016-02-22 20:31:31 -0500

    add click events for main page and albums

--- main.js
+++ main.js
@@ -1,33 +1,61 @@
 
 
-var togglePics = $("body").find("a");
-togglePics.click(function(event){
+$(".mainPage").on("click", "img", function(event) {
   event.preventDefault();
-  var selectedPage = "." + $(this).attr('rel');
-  $(selectedPage).siblings('div').addClass('inactive');
-  $(selectedPage).removeClass('inactive');
+  $("section").removeClass("active");
+  $(".albums").addClass("active");
+  var albumRel = $(this).parent().attr("rel");
+  var items = getAlbumPhotos(albumRel);
+  var photoDisplay = "";
+  items.photo.forEach(function(el) {
+    photoDisplay += '<a href="#" rel="">'
+    + '<img src="'+ el + '" alt="" />'
+  })
+    console.log(photoDisplay);
+    $(".albums-row-1").html(photoDisplay);
 });
+
+var getAlbumPhotos = function(albumClicked){
+  var photoArray = myPhotos.filter(function(el){
+    return el.albumName === albumClicked.trim();
+  }).pop();
+  return photoArray;
+}
+
+$(".multiAlbums").on("click","li", function(event){
+  event.preventDefault();
+  var pullAlbum = $(this).attr("rel");
+  var items = getAlbumPhotos(pullAlbum);
+  var photoDisplay = "";
+  items.photo.forEach(function(el) {
+    photoDisplay += '<a href="#" rel="">'
+    + '<img src="'+ el + '" alt="" />'
+  })
+    console.log(photoDisplay);
+    $(".albums-row-1").html(photoDisplay);
+})
+
+$("multiAlbums").on("click", "img", function(event){
+  event.preventDefault();
+  var bigImg = $(this).attr("rel");
+  var items = getAlbumPhotos(bigImg);
+  var singlePhoto = "";
+  items.photo.forEach(function(el) {
+    singlePhoto += '<img src="'+ el + '" alt="" />'
+
+  })
+  $("photo-container").html(singlePhoto);
+})
+
 
 
 
 var albumCoverStr = "";
-photos.forEach(function(el) {
+myPhotos.forEach(function(el) {
   albumCoverStr +=
-   '<a id="album-1" href="" rel="">'
-   + '<img src="'+ el.albumCover + '" alt="" />'
+   '<a href="#" rel="' + el.albumName + ' ">'
+   + '<img id="' + el.albumName + '" src="'+ el.albumCover + '" alt="" />'
    + '</a>'
-  + '</div>'
 });
 
-$('.mainPage-row-1').append(albumCoverStr);
-
-// var albumCoverStr2 = "";
-// photos.forEach(function(el) {
-//   albumCoverStr2 +=
-//    '<a id="album-1" href="">'
-//    + '<img src="'+ el.albumCover2 + '" alt="" />'
-//    + '</a>'
-//   + '</div>'
-// });
-//
-// $('.albums-row-1').append(albumCoverStr2);
+$(".mainPage-row-1").append(albumCoverStr);
--- photos.js
+++ photos.js
@@ -1,53 +1,71 @@
-var photos = [
+var myPhotos = [
   {
     albumName: "album-1",
     albumCover: 'photos/wife-wedding.jpg',
-    photos: [
+    photo: [
       'photos/wife-captwoodys.jpg',
-      'photos/wife-beach.jpg',
+      'photos/wife-heritage.jpg',
+      'photos/wife-bar.jpg',
+      'photos/wife-captwoodys.jpg',
+      'photos/wife-newhome.jpg',
       'photos/wife-bar.jpg'
     ]
   },
   {
-    albumName: "Album 2",
+    albumName: "Album-2",
     albumCover: 'photos/fam-oldschool.jpg',
-    photos: [
+    photo: [
+      'photos/11535700_10206319204213284_2865217785424381702_n.jpg',
+      'photos/fam-seattle.jpg',
+      'photos/fam-siblings-oldschool.jpg',
       'photos/11535700_10206319204213284_2865217785424381702_n.jpg',
       'photos/fam-seattle.jpg',
       'photos/fam-siblings-oldschool.jpg'
     ]
   },
   {
-    albumName: "Album 3",
+    albumName: "Album-3",
     albumCover: 'photos/sullivan-2.jpg',
-    photos: [
+    photo: [
       'photos/sullivan-2.jpg',
       'photos/woody-beach.jpg',
-      'photos/name-of-file3.jpg'
+      'photos/woody-beach.jpg',
+      'photos/sullivan-2.jpg',
+      'photos/woody-beach.jpg',
+      'photos/woody-beach.jpg'
     ]
   },
   {
-    albumName: "Album 4",
+    albumName: "Album-4",
     albumCover: 'photos/friends-kentucky.jpg',
-    photos: [
+    photo: [
+      'photos/friends-frostyfrog.jpg',
+      'photos/friends-bigjon.jpg',
+      'photos/friends-kurt.jpg',
       'photos/friends-frostyfrog.jpg',
       'photos/friends-bigjon.jpg',
       'photos/friends-kurt.jpg'
     ]
   },
   {
-    albumName: "Album 5",
-    albumCover: 'photos/wife-heritage.jpg',
-    photos: [
-      'photos/newhome.jpg',
-      'photos/uglysweater.jpg',
+    albumName: "Album-5",
+    albumCover: 'photos/wife-captwoodys.jpg',
+    photo: [
+      'photos/wife-captwoodys.jpg',
+      'photos/wife-heritage.jpg',
+      'photos/wife-bar.jpg',
+      'photos/wife-captwoodys.jpg',
+      'photos/wife-newhome.jpg',
       'photos/wife-bar.jpg'
     ]
   },
   {
-    albumName: "Album 6",
+    albumName: "Album-6",
     albumCover: 'photos/friends-highschool.jpg',
-    photos: [
+    photo: [
+      'photos/friends-frostyfrog.jpg',
+      'photos/friends-bigjon.jpg',
+      'photos/friends-kurt.jpg',
       'photos/friends-frostyfrog.jpg',
       'photos/friends-bigjon.jpg',
       'photos/friends-kurt.jpg'
